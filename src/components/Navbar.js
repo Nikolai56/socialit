c1e9fab3d698c01c2fef72539c39e62cb2f8131c
Nikolai <4or@mail.ru>
2018-07-24 01:17:38 +0300

    update links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => (
             </div>
             <ul className="menu_list">
                 <li><Link exact activeClassName="active" to="/">Главная</Link></li>
-                <li><Link exact activeClassName="active" to="/about">Новости</Link></li>
-                <li><Link exact activeClassName="active" to="/services">Услуги</Link></li>
-                <li><Link exact activeClassName="active" to="/products">Работы</Link></li>
-                <li><Link exact activeClassName="active" to="/products">Контакты</Link></li>
+                <li><Link exact activeClassName="active" to="/about/">Новости</Link></li>
+                <li><Link exact activeClassName="active" to="/services/">Услуги</Link></li>
+                <li><Link exact activeClassName="active" to="/products/">Работы</Link></li>
+                <li><Link exact activeClassName="active" to="/products/">Контакты</Link></li>
             </ul>
         </div>
     </nav>
