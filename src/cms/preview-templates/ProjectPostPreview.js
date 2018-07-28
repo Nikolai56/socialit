ae0d8ec75d85120ca0ac54759b23dad755db56a3
Nikolai <4or@mail.ru>
2018-07-28 21:31:58 +0300

    preview

---
+++ src/cms/preview-templates/ProjectPostPreview.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ProjectPostTemplate } from '../../templates/project-post'
+
+const ProjectPostPreview = ({ entry, widgetFor }) => {
+    const entryAuthors = entry.getIn(['data', 'authors']);
+    const authors = entryAuthors ? entryAuthors.toJS() : [];
+    return (
+        <ProjectPostTemplate
+            content={widgetFor('body')}
+            description={entry.getIn(['data', 'description'])}
+            title={entry.getIn(['data', 'title'])}
+            image={entry.getIn(['data', 'image'])}
+            authors={authors}
+        />
+    )
+};
+
+export default ProjectPostPreview
