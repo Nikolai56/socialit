00c290a74474ac4163c72d721a1d361a50e90bca
Nikolai <4or@mail.ru>
2018-07-29 13:11:05 +0300

    update layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import Header from '../components/Header'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import './reset.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import Header from '../components/Header';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import './reset.css';
 import './stylesheet.scss';
 import './modal.css';
 import './media.css';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Главная | SOCIALIT design" />
     <Header />
     <Navbar />
     {children()}
     <Footer />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
