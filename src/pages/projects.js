31c714ac721d2a5184696100990562146ae03e7e
Nikolai <4or@mail.ru>
2018-07-29 13:08:48 +0300

    update projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,35 +7,16 @@ export default class IndexPage extends React.Component {
     const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Проекты</h1>
-          </div>
+      <section className="portfolio">
+        <div className="container-fluid">
+            <h1>Работы</h1>
           {posts
             .filter(post => post.node.frontmatter.templateKey === 'project-post')
             .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
-              </div>
+                <Link key={post.id} to={post.fields.slug}>
+                    <img src={post.frontmatter.image} alt={`${post.frontmatter.title} | ${post.frontmatter.date}`} />
+                    {/*<p>{post.excerpt}</p>*/}
+                </Link>
             ))}
         </div>
       </section>
@@ -56,7 +37,7 @@ export const pageQuery = graphql`
           frontmatter {
             title
             templateKey
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "DD.MM.YYYY")
             image
           }
         }
