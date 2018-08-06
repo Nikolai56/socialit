8fda9c04d5504e440f6f5575ff4c6cd9923152c5
Nikolai <4or@mail.ru>
2018-08-06 12:52:24 +0300

    try Form Handling with a Stateful React Form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+};
+
 class Modal extends React.PureComponent {
     constructor () {
         super();
         this.state = {
-            showModal: false
+            showModal: false,
+            name: "", email: "", message: "",
         };
     }
 
@@ -22,7 +29,22 @@ class Modal extends React.PureComponent {
         this.setState({ showModal: false });
     };
 
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+        e.preventDefault();
+    };
+
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
     render () {
+        const { name, tel, comment } = this.state;
         return (
             <div className="yellow-button">
                 <a href="#" onClick={this.handleOpenModal}>Закажите обратный звонок</a>
@@ -48,17 +70,15 @@ class Modal extends React.PureComponent {
                         </div>
 
                         <div className="modal-body">
-                            {/*<form name="modal" method="POST" data-netlify netlify-honeypot="modal-bot-field">*/}
-                                {/*<input type="text" name="name" placeholder="Имя" required />*/}
-                                {/*<input type="tel" name="tel" placeholder="Телефон" required />*/}
-                                {/*<textarea name="comment" rows="4" placeholder="Комментарий" />*/}
+                            <form onSubmit={this.handleSubmit} netlify-honeypot="modal-bot-field">
+                                <input type="text" name="name" placeholder="Имя" value={name} onChange={this.handleChange} required />
+                                <input type="tel" name="tel" placeholder="Телефон" value={tel} onChange={this.handleChange} required />
+                                <textarea name="comment" rows="4" placeholder="Комментарий" value={comment} onChange={this.handleChange} />
 
-                                {/*/!*<input type="hidden" name="form-name" value="modal" />*!/*/}
-                                {/*<label className="hidden">Don’t fill this out if you're human: <input name="modal-bot-field" /></label>*/}
-
-                                {/*<button className="btn" type="submit">Заказать звонок</button>*/}
-
-                            {/*</form>*/}
+                                {/*<input type="hidden" name="form-name" value="modal" />*/}
+                                <label className="hidden">Don’t fill this out if you're human: <input name="modal-bot-field" /></label>
+                                <button className="btn" type="submit">Заказать звонок</button>
+                            </form>
                         </div>
 
                         <div className="modal-footer">
@@ -67,17 +87,17 @@ class Modal extends React.PureComponent {
 
                     </div>
                 </ReactModal>
-                <form name="modal" method="POST" data-netlify netlify-honeypot="modal-bot-field">
-                    <input type="text" name="name" placeholder="Имя" required />
-                    <input type="tel" name="tel" placeholder="Телефон" required />
-                    <textarea name="comment" rows="4" placeholder="Комментарий" />
+                {/*<form name="modal" method="POST" data-netlify netlify-honeypot="modal-bot-field">*/}
+                    {/*<input type="text" name="name" placeholder="Имя" required />*/}
+                    {/*<input type="tel" name="tel" placeholder="Телефон" required />*/}
+                    {/*<textarea name="comment" rows="4" placeholder="Комментарий" />*/}
 
-                    {/*<input type="hidden" name="form-name" value="modal" />*/}
-                    <label className="hidden">Don’t fill this out if you're human: <input name="modal-bot-field" /></label>
+                    {/*/!*<input type="hidden" name="form-name" value="modal" />*!/*/}
+                    {/*<label className="hidden">Don’t fill this out if you're human: <input name="modal-bot-field" /></label>*/}
 
-                    <button className="btn" type="submit">Заказать звонок</button>
+                    {/*<button className="btn" type="submit">Заказать звонок</button>*/}
 
-                </form>
+                {/*</form>*/}
             </div>
         );
     }
