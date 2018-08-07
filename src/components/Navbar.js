0ece8be683dbc8bea41df49849fcdc69a65f569f
Nikolai <4or@mail.ru>
2018-08-07 18:58:21 +0300

    added menu toggle on small screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,38 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Modal from './Modal';
 
-const Navbar = () => (
-    <nav className="nav1 clearfix">
-        <div className="container">
-            <Modal />
-            <div className="menu_toggle">
-                Меню
+const Navbar = () => {
+    function handleToggleMenu() {
+        const menu = document.querySelector('.menu_list');
+        if (menu.classList.contains('open')) {
+            menu.classList.remove('open');
+        } else {
+            menu.classList.add('open');
+        }
+    }
+    function handleCloseMenu() {
+        const menu = document.querySelectorAll('.menu_list')[0];
+        menu.classList.remove('open');
+    }
+    
+    return (
+        <nav className="nav1 clearfix">
+            <div className="container">
+                <Modal />
+                <div className="menu_toggle" onClick={handleToggleMenu}>
+                    Меню
+                </div>
+                <ul className="menu_list">
+                    {/*<li><Link exact activeClassName="active" to="/">Главная</Link></li>*/}
+                    {/*<li><Link exact activeClassName="active" to="/about/">Новости</Link></li>*/}
+                    <li><Link onClick={handleCloseMenu} exact activeClassName="active" to="/services/">Услуги</Link></li>
+                    <li><Link onClick={handleCloseMenu} exact activeClassName="active" to="/">Работы</Link></li>
+                    <li><Link onClick={handleCloseMenu} exact activeClassName="active" to="/contacts/">Контакты</Link></li>
+                    {/*<li><Link exact activeClassName="active" to="/products/">Контакты</Link></li>*/}
+                </ul>
             </div>
-            <ul className="menu_list">
-                {/*<li><Link exact activeClassName="active" to="/">Главная</Link></li>*/}
-                {/*<li><Link exact activeClassName="active" to="/about/">Новости</Link></li>*/}
-                <li><Link exact activeClassName="active" to="/services/">Услуги</Link></li>
-                <li><Link exact activeClassName="active" to="/">Работы</Link></li>
-                <li><Link exact activeClassName="active" to="/contacts/">Контакты</Link></li>
-                {/*<li><Link exact activeClassName="active" to="/products/">Контакты</Link></li>*/}
-            </ul>
-        </div>
-    </nav>
-);
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
