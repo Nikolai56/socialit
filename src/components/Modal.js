8b78087a75a0b6d4b08cd19894ff75565f9bdebb
Nikolai <4or@mail.ru>
2018-08-07 11:58:40 +0300

    removed honeypot from modal form

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,7 +15,6 @@ class Modal extends React.PureComponent {
             name: '',
             email: '',
             message: '',
-            bot: '',
         };
     }
 
@@ -36,8 +35,7 @@ class Modal extends React.PureComponent {
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "modal", "honeypot": "bot",
-                "data-netlify-honeypot": "bot", "form-data-netlify-honeypot": "bot", ...this.state })
+            body: encode({ "form-name": "modal", ...this.state })
         })
             .then(() => {
                 alert("Заявка отправлена!");
@@ -51,7 +49,7 @@ class Modal extends React.PureComponent {
     handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render () {
-        const { name, tel, comment, bot } = this.state;
+        const { name, tel, comment } = this.state;
         return (
             <div className="yellow-button">
                 <a href="#" onClick={this.handleOpenModal}>Закажите обратный звонок</a>
@@ -76,13 +74,10 @@ class Modal extends React.PureComponent {
                             <h3>Закажите обратный звонок и мы вам перезвоним.</h3>
                         </div>
                         <div className="modal-body">
-                            <form onSubmit={this.handleSubmit} netlify-honeypot="bot">
+                            <form onSubmit={this.handleSubmit}>
                                 <input type="text" name="name" placeholder="Имя" value={name} onChange={this.handleChange} required />
                                 <input type="tel" name="tel" placeholder="Телефон" value={tel} onChange={this.handleChange} required />
                                 <textarea name="comment" rows="4" placeholder="Комментарий" value={comment} onChange={this.handleChange} />
-                                <label className="hidden">Don’t fill this out if you're human:
-                                    <input name="bot" value={bot} onChange={this.handleChange} />
-                                </label>
                                 <button className="btn" type="submit">Заказать звонок</button>
                             </form>
                         </div>
@@ -91,17 +86,6 @@ class Modal extends React.PureComponent {
                         </div>
                     </div>
                 </ReactModal>
-                {/*<form name="modal" method="POST" data-netlify netlify-honeypot="modal-bot-field">*/}
-                    {/*<input type="text" name="name" placeholder="Имя" required />*/}
-                    {/*<input type="tel" name="tel" placeholder="Телефон" required />*/}
-                    {/*<textarea name="comment" rows="4" placeholder="Комментарий" />*/}
-
-                    {/*/!*<input type="hidden" name="form-name" value="modal" />*!/*/}
-                    {/*<label className="hidden">Don’t fill this out if you're human: <input name="modal-bot-field" /></label>*/}
-
-                    {/*<button className="btn" type="submit">Заказать звонок</button>*/}
-
-                {/*</form>*/}
             </div>
         );
     }
